3d49a22edece4d067c15c41ec7ec8433c916a8ae
Vaidik Choudhary <vaidikchoudhary771@gmail.com>
2026-03-08 23:14:05 +0530

    [Vaidik Choudhary] : updated AddressBookServiceTest.java file

--- src/test/java/com/vaidik/addressbook/AddressBookServiceTest.java
+++ src/test/java/com/vaidik/addressbook/AddressBookServiceTest.java
@@ -6,6 +6,7 @@ import com.vaidik.addressbook.model.Contact;
 import com.vaidik.addressbook.service.AddressBookService;
 import com.vaidik.addressbook.util.CSVUtil;
 import com.vaidik.addressbook.util.FileUtil;
+import com.vaidik.addressbook.util.JSONUtil;
 
 import org.junit.jupiter.api.*;
 
@@ -15,6 +16,7 @@ import java.util.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AddressBookServiceTest {
+
 
 
     private Contact createContact() {
@@ -960,6 +962,71 @@ public class AddressBookServiceTest {
     public void givenEmptyCSV_whenRead_shouldReturnEmptyList() {
 
         List<Contact> contacts = CSVUtil.readContactsFromCSV("src/test/resources/testdata/empty.csv");
+
+        assertEquals(0, contacts.size());
+    }
+    
+    @Test
+    public void givenContacts_whenSavedToJSON_shouldCreateFile() {
+
+        AddressBookService service = new AddressBookService();
+
+        service.addContact("personal",
+                new Contact("Tarus","Prabhat","","Ariana","Geornite","567834","",""));
+
+        String path = "src/test/resources/testdata/test_contacts.json";
+
+        service.saveContactsToJSON("personal", path);
+
+        File file = new File(path);
+
+        assertTrue(file.exists());
+    }
+    
+    @Test
+    public void givenJSONFile_whenRead_shouldReturnContacts() {
+
+        List<Contact> contacts = List.of(
+                new Contact("Tarus","Prabhat","","Ariana","Geornite","567834","","")
+        );
+
+        JSONUtil.writeContactsToJSON(
+                "src/test/resources/testdata/test_contacts.json",
+                contacts
+        );
+
+        List<Contact> result = JSONUtil.readContactsFromJSON(
+                "src/test/resources/testdata/test_contacts.json"
+        );
+
+        assertEquals(1, result.size());
+    }
+    
+    @Test
+    public void givenMultipleContacts_whenSavedAndLoadedJSON_shouldMatchCount() {
+
+        List<Contact> contacts = List.of(
+                new Contact("Tarus","Prabhat","","Ariana","Geornite","567834","",""),
+                new Contact("Rahul","Verma","","Delhi","DL","110001","","")
+        );
+
+        String path = "src/test/resources/testdata/multi_contacts.json";
+
+        JSONUtil.writeContactsToJSON(path, contacts);
+
+        List<Contact> result = JSONUtil.readContactsFromJSON(path);
+
+        assertEquals(2, result.size());
+    }
+    
+    @Test
+    public void givenEmptyJSON_whenRead_shouldReturnEmptyList() {
+
+        String path = "src/test/resources/testdata/empty.json";
+
+        JSONUtil.writeContactsToJSON(path, new ArrayList<>());
+
+        List<Contact> contacts = JSONUtil.readContactsFromJSON(path);
 
         assertEquals(0, contacts.size());
     }
