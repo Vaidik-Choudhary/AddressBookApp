74cc5447ffb7c5d8b243a8a2ba93d44247fa9094
Vaidik Choudhary <vaidikchoudhary771@gmail.com>
2026-03-08 20:14:27 +0530

    [Vaidik Choudhary] : Updated AddressBookController.java file

--- src/main/java/com/vaidik/addressbook/controller/AddressBookController.java
+++ src/main/java/com/vaidik/addressbook/controller/AddressBookController.java
@@ -3,6 +3,7 @@ package com.vaidik.addressbook.controller;
 import com.vaidik.addressbook.model.Contact;
 import com.vaidik.addressbook.service.AddressBookService;
 import org.springframework.web.bind.annotation.*;
+import java.util.*;
 
 @RestController
 @RequestMapping("/addressbooks")
@@ -45,5 +46,11 @@ public class AddressBookController {
         }
 
         return "Contact not found";
+    }
+    
+    @GetMapping("/{bookName}/contacts")
+    public List<Contact> getContacts(@PathVariable String bookName) {
+
+        return service.getContacts(bookName);
     }
 }
