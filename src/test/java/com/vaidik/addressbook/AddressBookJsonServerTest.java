18e2ba15ef4f6546cd0c16113dee41bf6ed88659
Vaidik Choudhary <vaidikchoudhary771@gmail.com>
2026-03-09 10:02:24 +0530

    [Vaidik Choudhary] : updated AddressBookJsonServerTest.java file

--- src/test/java/com/vaidik/addressbook/AddressBookJsonServerTest.java
+++ src/test/java/com/vaidik/addressbook/AddressBookJsonServerTest.java
@@ -8,7 +8,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class AddressBookJsonServerTest {
 
-
 	
     @Test
     public void givenJSONServer_whenContactsFetched_shouldReturnRecords() {
@@ -17,6 +16,34 @@ public class AddressBookJsonServerTest {
 
         assertNotNull(response);
         assertEquals(200, response.getStatusCode());
+
+    }
+    
+    @Test
+    public void givenNewContact_whenAddedToJsonServer_shouldReturnCreated() {
+
+        String newContact = """
+            {
+              "firstName": "Amit",
+              "lastName": "Sharma",
+              "address": "",
+              "city": "Mumbai",
+              "state": "MH",
+              "zip": "400001",
+              "phoneNumber": "7777777777",
+              "email": "amit@gmail.com"
+            }
+            """;
+
+        Response response =
+                RestAssured
+                        .given()
+                        .contentType("application/json")
+                        .body(newContact)
+                        .when()
+                        .post("http://localhost:3000/contacts");
+
+        assertEquals(201, response.getStatusCode());
 
     }
 }
